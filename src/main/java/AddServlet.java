027e3c3b5d957ce1aab37cbf1358d8f3bad2563d
scm-yza <scm.yezawaung@gmail.com>
2023-03-06 14:34:57 +0630

    add first servlet

---
+++ src/main/java/AddServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+  public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+    int number1 = Integer.parseInt(req.getParameter("num1"));
+    int number2 = Integer.parseInt(req.getParameter("num2"));
+
+    int output = number1 + number2;
+    res.getWriter().println("Result " + output);
+  } 
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
+<!DOCTYPE html>
 <html>
 <body>
-  <h2>Hello World!</h2>
+  <form action="add" method="post">
+    Enter 1st number: <input type="text" name="num1"><br />
+    Enter 2nd number: <input type="text" name="num2"><br />
+    <input type="submit">
+  </form>
 </body>
 </html>
