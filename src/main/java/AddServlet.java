45a2ed4a175fc2a32ca61317430bce177f9565fe
scm-yza <scm.yezawaung@gmail.com>
2023-03-06 15:07:38 +0630

    apply first RequestDispatcher

--- src/main/java/AddServlet.java
+++ src/main/java/AddServlet.java
@@ -1,17 +1,22 @@
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 
-  public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+  public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
     int number1 = Integer.parseInt(req.getParameter("num1"));
     int number2 = Integer.parseInt(req.getParameter("num2"));
 
     int output = number1 + number2;
-    res.getWriter().println("Result " + output);
+
+    RequestDispatcher dispatcher = req.getRequestDispatcher("square");
+    req.setAttribute("k", output);    
+    dispatcher.forward(req, res);
   } 
 }
---
+++ src/main/java/SqServlet.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet  extends HttpServlet {
+    
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        
+        int k = (int) req.getAttribute("k");
+        k = k * k;
+        res.getWriter().println("Result " + k);
+    }
+}
