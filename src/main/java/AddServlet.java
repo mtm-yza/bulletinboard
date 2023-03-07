8208852948c08a31bd2a53680a85e13b06548489
scm-yza <scm.yezawaung@gmail.com>
2023-03-07 08:13:15 +0630

    applying servlet annotation

--- src/main/java/AddServlet.java
+++ src/main/java/AddServlet.java
@@ -1,22 +1,19 @@
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 
-  public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
-    int number1 = Integer.parseInt(req.getParameter("num1"));
-    int number2 = Integer.parseInt(req.getParameter("num2"));
+        int number1 = Integer.parseInt(req.getParameter("num1"));
+        int number2 = Integer.parseInt(req.getParameter("num2"));
 
-    int output = number1 + number2;
-
-    RequestDispatcher dispatcher = req.getRequestDispatcher("square");
-    req.setAttribute("k", output);    
-    dispatcher.forward(req, res);
-  } 
+        int output = number1 + number2;
+        res.getWriter().println("Result: " + output);
+    }
 }
--- src/main/java/SqServlet.java
+++ src/main/java/SqServlet.java
@@ -1,15 +1,17 @@
 import java.io.IOException;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/square")
 public class SqServlet  extends HttpServlet {
     
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         
         int k = (int) req.getAttribute("k");
         k = k * k;
-        res.getWriter().println("Result " + k);
+        res.getWriter().println("Result: " + k);
     }
 }
